821d8bdaef800dade7596947eeeaae06f1cfe428
sujitapandeyy <sujitapandey806@gmail.com>
2024-03-20 22:14:54 +0545

    work on index

--- app/page.js
+++ app/page.js
@@ -1,10 +1,18 @@
+import axios from "axios";
+const res=await axios.get("https://jsonplaceholder.typicode.com/posts")
+const response=res.data;
 
 export default function Home() {
   return (
      <main>
-      im sujita
+      {
+        response.map((post)=>{
+          const {userId,id,title,body}=post;
+          return (
+           <p key={id}>{id}{title}</p>
+          )
+        })
+      }
      </main>
-     
-     
   );
 }
